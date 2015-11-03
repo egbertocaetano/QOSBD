16d7272bab25f20a64de54504b44e4a3fd324568
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-11-03 18:04:49 -0200

    first version

--- teste.c
+++ teste.c
@@ -11,7 +11,9 @@
 #include <linux/fs.h>
 
 #define BLOCKSIZE 512
-#define TIMEOUT 30
+#define TIMEOUT 10
+
+typedef unsigned long long int u_long_long;
 
 int count;
 time_t start;
@@ -42,8 +44,7 @@ void handle(const char *string, int error)
 		exit(EXIT_FAILURE);
 	}
 }
-
-int main(int argc, char **argv)
+void test_random(int argc, char **argv)
 {
 	char buffer[BLOCKSIZE];
 	int fd, retval;
@@ -65,7 +66,7 @@ int main(int argc, char **argv)
 
 	retval = ioctl(fd, BLKGETSIZE, &numblocks);
 	handle("ioctl", retval == -1);
-	printf("Benchmarking %s [%luMB], wait %d seconds",
+	printf("Benchmarking %s [%luMB], wait %d seconds\n",
 	       argv[1], numblocks / 2048, TIMEOUT);
 
 	time(&start);
@@ -73,7 +74,11 @@ int main(int argc, char **argv)
 	signal(SIGALRM, &done);
 	alarm(1);
 
-	for (;;) {
+//	for (;;) {
+	u_long_long i = 0;
+	lseek64(fd, 0, SEEK_SET);
+	for (i = 0; ; i++) {
+		//printf("pos[%llu]: %llu\n", i, (u_long_long) lseek64(fd, 0, SEEK_CUR));
 		offset = (off64_t) numblocks * random() / RAND_MAX;
 		retval = lseek64(fd, BLOCKSIZE * offset, SEEK_SET);
 		handle("lseek64", retval == (off64_t) -1);
@@ -82,4 +87,55 @@ int main(int argc, char **argv)
 		count++;
 	}
 	/* notreached */
+}
+void test_sequential(int argc, char **argv)
+{
+	char buffer[BLOCKSIZE];
+	int fd, retval;
+	u_long_long numblocks;
+	off64_t offset;
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	printf("Seeker v2.0, 2007-01-15, "
+	       "http://www.linuxinsight.com/how_fast_is_your_disk.html\n");
+
+	if (argc != 2) {
+		printf("Usage: seeker <raw disk device>\n");
+		exit(EXIT_SUCCESS);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	handle("open", fd < 0);
+
+	retval = ioctl(fd, BLKGETSIZE, &numblocks);
+	handle("ioctl", retval == -1);
+	printf("Benchmarking %s [%luMB], wait %d seconds\n",
+	       argv[1], numblocks / 2048, TIMEOUT);
+
+	time(&start);
+	srand(start);
+	signal(SIGALRM, &done);
+	alarm(1);
+
+	u_long_long i = numblocks * random() / RAND_MAX;
+	lseek64(fd, 0, SEEK_SET);
+	for (; i < numblocks; i++) {
+		offset = (off64_t) i;
+	//	printf("pos[%llu]: %llu\n", i, (u_long_long) lseek64(fd, 0, SEEK_CUR));
+	//	printf("pos[%llu]: %llu\n", i, (u_long_long) offset);
+		retval = lseek64(fd, BLOCKSIZE * offset, SEEK_SET);
+		handle("lseek64", retval == (off64_t) -1);
+		retval = read(fd, buffer, BLOCKSIZE);
+		handle("read", retval < 0);
+		count++;
+	}
+	/* notreached */
+}
+int main(int argc, char **argv)
+{
+	//test_random(argc, argv);
+	test_sequential(argc, argv);
+
+	return 0;
 }
