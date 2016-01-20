7523e523ae2c7a52a7189a3e1093e6ac4cc56c4a
Miguel Mendes <miguel.mdebrito@gmail.com>
2016-01-20 09:59:58 -0200

    latest update

--- teste.c
+++ teste.c
@@ -16,21 +16,14 @@
 
 typedef unsigned long long int u_long_long;
 
-#define PAGE_SIZE 1000
-#define N_PAGES 100000L
-#define BLOCKSIZE 512
+#define BLOCKSIZE 4096
 
-char BLANK_PAGE[PAGE_SIZE];
-
-u_long_long NUM_OF_PAGES = N_PAGES;
+u_long_long NUM_OF_BLOCKS = 1000000L;
 u_long_long FILE_SIZE = 0;
 
 void fill_indexes(u_long_long arr[], u_long_long n);
-void read_sequentially(int fd, char page[], u_long_long offsets[]);
-void read_random(int fd, char page[], u_long_long offsets[]);
 void shuffle(u_long_long *array, u_long_long n);
 char *preallocate_test_file(char filename[], char end[]);
-void shuffle_page(char *array, int n);
 void handle(const char *string, int error)
 {
     if (error) {
@@ -46,18 +39,30 @@ off64_t fsize(const char *filename) {
 
     return -1;
 }
-void run_seq(int fd, unsigned long numblocks, off64_t offset);
-void run_random(int fd, unsigned long numblocks, off64_t offset);
+void run_seq(int fd, u_long_long offsets[]);
+void run_random(int fd, u_long_long offsets[]);
 int main(int argc, char **argv)
 {
     int fd, retval;
     unsigned long numblocks;
     off64_t offset;
 
+
+    u_long_long *offsets = malloc(NUM_OF_BLOCKS * sizeof(u_long_long));
+
+    fill_indexes(offsets, NUM_OF_BLOCKS);
+
+    printf("NUM_OF_BLOCKS: %llu\n", NUM_OF_BLOCKS);
+    printf("FILE_SIZE: %llu\n", NUM_OF_BLOCKS * BLOCKSIZE);
+    printf("BLOCKSIZE: %i\n", BLOCKSIZE);
+
+    shuffle(offsets, NUM_OF_BLOCKS);
+
+
     setvbuf(stdout, NULL, _IONBF, 0);
 
     if (argc != 2) {
-        printf("Usage: seeker <raw disk device>\n");
+        printf("Usage: sudo teste <raw disk device>\n");
         exit(EXIT_SUCCESS);
     }
 
@@ -72,63 +77,61 @@ int main(int argc, char **argv)
 
     gettimeofday(&tv1, NULL);
     printf("Sequential read started...\n");
-    run_seq(fd, numblocks, offset);
+    run_seq(fd, offsets);
     gettimeofday(&tv2, NULL);
 
     double seq_time = (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
              (double) (tv2.tv_sec - tv1.tv_sec);
 
-    printf("Sequential read total time = %f seconds\n", seq_time);
+    printf("Sequential read total time = %f seconds\n\n", seq_time);
 
 
 
-    gettimeofday(&tv1, NULL);
+/*    gettimeofday(&tv1, NULL);
     printf("Random read started...\n");
-    run_random(fd, numblocks, offset);
+    run_random(fd, offsets);
     gettimeofday(&tv2, NULL);
 
     double rand_time = (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
              (double) (tv2.tv_sec - tv1.tv_sec);
 
     printf("Random read total time = %f seconds\n", rand_time);
+*/
+    free(offsets);
 
     return 0;
 }
 
-void run_seq(int fd, unsigned long numblocks, off64_t offset) 
+void run_seq(int fd, u_long_long offsets[]) 
 {
     int retval;
     char buffer[BLOCKSIZE];
     u_long_long i = 0;
     lseek64(fd, 0, SEEK_SET);
-    for (i = 0; i < 1000000; i++) {
-        //printf("pos[%llu]: %llu\n", i, (u_long_long) lseek64(fd, 0, SEEK_CUR));
-
+    for (i = 0; i < NUM_OF_BLOCKS; i++) {
+        // printf("pos[%llu]: %llu\n", i, (u_long_long) lseek64(fd, 0, SEEK_CUR));
         handle("lseek64", retval == (off64_t) -1);
         retval = read(fd, buffer, BLOCKSIZE);
         handle("read", retval < 0);
     }
 }
 
-void run_random(int fd, unsigned long numblocks, off64_t offset) 
+void run_random(int fd, u_long_long offsets[]) 
 {
     int retval;
     char buffer[BLOCKSIZE];
     u_long_long i = 0;
     lseek64(fd, 0, SEEK_SET);
-    for (i = 0; i < 1000000; i++) {
-        //printf("pos[%llu]: %llu\n", i, (u_long_long) lseek64(fd, 0, SEEK_CUR));
+    for (i = 0; i < NUM_OF_BLOCKS; i++) {
+        // printf("pos[%llu]: %llu\n", i, (u_long_long) lseek64(fd, 0, SEEK_CUR));
         // printf("num of blocks %llu\n", numblocks);
-        offset = (off64_t) (numblocks/1024) * random() / RAND_MAX;
         // printf("pos[%llu]: %llu\n", offset);
-        retval = lseek64(fd, BLOCKSIZE * offset, SEEK_SET);
+        retval = lseek64(fd, BLOCKSIZE * offsets[i], SEEK_SET);
         handle("lseek64", retval == (off64_t) -1);
         retval = read(fd, buffer, BLOCKSIZE);
         handle("read", retval < 0);
     }
 }
-
-
 
 void fill_indexes(u_long_long arr[], u_long_long n)
 {
@@ -136,39 +139,7 @@ void fill_indexes(u_long_long arr[], u_long_long n)
     for (i = 0; i < n; i++)
         arr[i] = i;
 }
-void read_sequentially(int fd, char page[], u_long_long offsets[])
-{
-    u_long_long i;
-    u_long_long retval = 0;
-    
-    lseek64(fd, 0, SEEK_SET);
 
-    for (i = 0; i < NUM_OF_PAGES; i++) {
-        // retval = lseek64(fd, i * PAGE_SIZE, SEEK_SET);
-        // printf("pos: %llu\n", (u_long_long) lseek(fd, 0, SEEK_CUR));
-        //printf("i: %llu\n", i);
-        handle("lseek64", retval == (off_t) - 1);
-        retval = read(fd, page, PAGE_SIZE);
-        handle("read", retval < 0);
-    }
-}
-void read_random(int fd, char page[], u_long_long offsets[])
-{
-    u_long_long i;
-    u_long_long retval = 0;
-
-    lseek64(fd, 0, SEEK_SET);
-
-    for (i = 0; i < NUM_OF_PAGES; i++) {
-        //printf("pos: %llu\n", (u_long_long) lseek(fd, 0, SEEK_CUR));
-        //printf("i: %llu\n", i);
-        retval = lseek64(fd, offsets[i] * PAGE_SIZE, SEEK_SET);
-        // printf("i: %llu\n", offsets[i]);
-        handle("lseek64", retval == (off_t) - 1);
-        retval = read(fd, page, PAGE_SIZE);
-        handle("read", retval < 0);
-    }
-}
 void shuffle(u_long_long *array, u_long_long n)
 {
     srand(time(NULL));
@@ -181,58 +152,4 @@ void shuffle(u_long_long *array, u_long_long n)
             array[i] = t;
         }
     }
-}
-void shuffle_page(char *array, int n)
-{
-    srand(time(NULL));
-    if (n > 1) {
-        int i;
-        for (i = 0; i < n - 1; i++) {
-            int j = i + rand() / (RAND_MAX / (n - i) + 1);
-            int t = array[j];
-            array[j] = array[i];
-            array[i] = t;
-        }
-    }
-}
-
-char *preallocate_test_file(char filename[], char end[])
-{
-    FILE *fp;
-
-    char *page = malloc(PAGE_SIZE);
-
-    char *new_filename = malloc(strlen(filename) + strlen(end) + 1);
-
-    strcpy(new_filename,  filename);
-    strcat(new_filename, end);
-
-    printf("%s\n", new_filename);
-
-    if(access(new_filename, F_OK ) != -1 ) {
-        printf("file: %s already exists\n", new_filename);
-    } else {
-        printf("creating a new file: %s...\n", new_filename);
-        int i = 0;
-        for (i = 0; i < PAGE_SIZE; i++) {
-            page[i] = ((i + 65) % 122);
-        }
-
-        if ((fp = fopen(new_filename, "w+")) == NULL) {
-            printf("Couldn't create new file\n");
-            exit(1);
-        } 
-
-        u_long_long j;
-        for (j = 0; j < FILE_SIZE; j += PAGE_SIZE) {
-            shuffle_page(page, PAGE_SIZE);
-            fwrite(page, PAGE_SIZE, 1, fp);
-        }
-
-        fclose(fp);     
-    }
-
-    free(page);
-
-    return new_filename;
 }
