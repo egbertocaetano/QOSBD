9915716e10d6d0b70610ebd38ea32cfe10518bbe
Miguel Mendes <miguel.mdebrito@gmail.com>
2016-04-17 19:44:10 -0300

    script to perform all tests

---
+++ tools/run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+make
+
+NUM_OF_PAGES=(200000 400000 600000 800000 1000000 2000000)
+FILE_SIZES=(1024 2048 3072 4096 5120 102400)
+
+echo 'Running all tests...'
+for i in {0..5}
+do
+   echo "test number $i..."
+    for j in {1..5}
+    do
+        echo "FILE SIZE: ${FILE_SIZES[$j]}"
+        echo "NUMBER OF PAGES: ${NUM_OF_PAGES[$j]}"
+        # echo "$(pwd)"
+        ./qosdb file${FILE_SIZES[$i]} ${NUM_OF_PAGES[$i]}
+    done
+    rm file*
+done
+
